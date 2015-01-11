03e0e2c4e17334c08245ed3fb8d1707352e6ba45
Pavel Prokopenko <pavel.a.prokopenko@gmail.com>
2015-01-11 14:58:14 +0200

    add initial gh-pages

--- src/om_datepicker/events.cljs
+++
@@ -1,8 +0,0 @@
-(ns om-datepicker.events
-  (:require [cljs.core.async :as async :refer [chan put!]]
-            [goog.events :as events]))
-
-(defn mouse-click []
-  (let [ch (chan)]
-    (events/listen js/document events/EventType.CLICK #(put! ch %))
-    ch))
