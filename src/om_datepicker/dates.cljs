11d686fb0f6f9de19e0515b8640656c115b682af
Pavel Prokopenko <pavel.a.prokopenko@gmail.com>
2015-03-31 19:15:07 +0300

    improve date related functions

--- src/om_datepicker/dates.cljs
+++ src/om_datepicker/dates.cljs
@@ -21,13 +21,31 @@
   []
   (first-of-month (js/Date.)))
 
+(defn leap-year?
+  [year]
+  (or (and (= 0 (mod year 4))
+           (not (= 0 (mod year 100))))
+      (= 0 (mod year 400))))
+
+(defn days-in-month
+  [year month]
+  (get [31 (if (leap-year? year) 29 28) 31 30 31 30 31 31 30 31 30 31] month))
+
+(defn add-months
+  [date months]
+  (let [day  (.getDate date)
+        date (js/Date. (.getFullYear date)
+                       (+ (.getMonth date) months) 1)]
+    (.setDate date (min day (days-in-month (.getFullYear date) (.getMonth date))))
+    date))
+
 (defn next-month
   [date]
-  (js/Date. (.getFullYear date) (inc (.getMonth date)) (.getDate date)))
+  (add-months date 1))
 
 (defn previous-month
   [date]
-  (js/Date. (.getFullYear date) (dec (.getMonth date)) (.getDate date)))
+  (add-months date -1))
 
 (defn coerse-date
   [date]
@@ -38,6 +56,7 @@
 (defprotocol DateTimeProtocol
   (after? [this that])
   (before? [this that])
+  (between? [this start end])
   (is-future? [this]))
 
 (extend-protocol DateTimeProtocol
@@ -47,6 +66,12 @@
 
   (before? [this that]
     (<= (.getTime this) (.getTime that)))
+
+  (between? [this start end]
+    (and (>= (.getTime this)
+             (.getTime start))
+         (<= (.getTime this)
+             (.getTime end))))
 
   (is-future? [this]
     (> (.getTime this) (.getTime (js/Date.)))))
