35ff8a02d55fda23bc44f24fdfb9cf70ddd9f102
Pavel Prokopenko <pavel.a.prokopenko@gmail.com>
2015-10-14 21:20:09 +0300

    Version 0.0.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject om-datepicker "0.0.6-SNAPSHOT"
+(defproject om-datepicker "0.0.6"
   :description "a collection of various date/month picker components for Om"
   :url "http://github.com/prokpa/om-datepicker"
 
